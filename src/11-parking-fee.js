834a8bdd128c55262ec8e2dee7437320efc3ea59
viraljain7 <veerljain1234@gmail.com>
2026-02-10 00:58:59 +0530

    solve: City Central Parking

--- src/11-parking-fee.js
+++ src/11-parking-fee.js
@@ -34,4 +34,57 @@
  */
 export function calculateParkingFee(hours, vehicleType) {
   // Your code here
+  // Conditionals: https://classroom.github.com/a/fwCZoRgs
+  // Datatypes Foundation: https://classroom.github.com/a/LXgTzkVh
+  // Datatypes Intermediate: https://classroom.github.com/a/ARempKK3
+  // Loops: https://classroom.github.com/a/pZ1cSgDU
+  // Functions: https://classroom.github.com/a/o2RfZZTy
+
+  let parkingFees = 0;
+
+  let vehicleRange = ["car", "motorcycle", "bus"];
+  if (!vehicleRange.includes(vehicleType)) return -1;
+  hours = Math.ceil(hours);
+
+  if (hours < 1) return -1;
+
+  if (vehicleType === "car") {
+    while (hours > 0) {
+      if (hours > 1) {
+        parkingFees += 3;
+      } else {
+        parkingFees += 5;
+      }
+
+      if (parkingFees > 30) parkingFees = 30;
+      hours--;
+    }
+  } else if (vehicleType === "motorcycle") {
+    while (hours > 0) {
+      if (hours > 1) {
+        parkingFees += 2;
+      } else {
+        parkingFees += 3;
+      }
+
+      if (parkingFees > 18) parkingFees = 18;
+      hours--;
+    }
+  } else {
+    while (hours > 0) {
+      if (hours > 1) {
+        parkingFees += 7;
+      } else {
+        parkingFees += 10;
+      }
+
+      if (parkingFees > 60) parkingFees = 60;
+      hours--;
+    }
+  }
+
+  console.log(parkingFees);
+  return parkingFees;
 }
+
+calculateParkingFee(10, "bus");
